8e20cc448bf75bdd08683c8a5f8af59a40821131
lhzd863 <lhzd863@126.com>
2019-07-10 14:36:46 +0800

    Create tconsumer.go

---
+++ doc-nsq/tconsumer.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "log"
+    "github.com/nsqio/go-nsq"
+    "time"
+)
+
+func main() {
+    config :=nsq.NewConfig()
+    q,err := nsq.NewConsumer("write_test","ch",config)
+    if err !=nil{
+        log.Panic("Could not create consumer.")
+    }
+    defer q.Stop()
+    q.AddHandler(nsq.HandlerFunc(func(message *nsq.Message) error{
+        log.Printf("Got a message: %v",string(message.Body))
+        time.Sleep(5*time.Second)
+        return nil
+    }))
+    //err = q.ConnectToNSQD("192.168.9.111:32771");
+    err = q.ConnectToNSQD("172.18.18.99:4150");
+    if err !=nil {
+        log.Panic("Could not connect")
+    }
+    time.Sleep(3600*time.Second)
+}
