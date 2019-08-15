abf49677fd32b71add4d7e202bedb3bb513ef962
lhzd863 <lhzd863@126.com>
2019-08-15 21:50:48 +0800

    Update nodejs-linux.js

--- doc-nodejs/nodejs-linux.js
+++ doc-nodejs/nodejs-linux.js
@@ -1,3 +1,7 @@
 #
-wget 
+wget https://nodejs.org/dist/v12.8.0/node-v12.8.0-linux-x64.tar.xz
+xz -d node-v12.8.0-linux-x64.tar.xz
 mv node-v12.8.0-linux-x64 nodejs
+mv nodejs /usr/local/
+ln -s /usr/local/nodejs/bin/node /usr/local/bin
+ln -s /usr/local/nodejs/bin/npm /usr/local/bin
