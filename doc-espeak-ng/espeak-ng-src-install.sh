c763d1b8e0976ce9a62cd85978c7916dde9d566a
lhzd863 <zdwst618@126.com>
2021-01-11 13:44:36 +0800

    Update espeak-ng-src-install.sh

--- doc-espeak-ng/espeak-ng-src-install.sh
+++ doc-espeak-ng/espeak-ng-src-install.sh
@@ -2,6 +2,7 @@
 apt-get install gcc
 apt-get install make autoconf automake libtool pkg-config
 tar -zxvf espeak-ng-1.50.tgz
+cd espeak-ng
 ./autogen.sh
 ./configure --prefix=/usr --with-extdict-zh=yes
 make LIBDIR=/usr/lib/x86_64-linux-gnu install
