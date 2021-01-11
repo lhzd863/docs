c959297e66c0785aa8412b8a240e5d27a97fb6f9
lhzd863 <zdwst618@126.com>
2021-01-11 13:44:05 +0800

    Update espeak-ng-src-install.sh

--- doc-espeak-ng/espeak-ng-src-install.sh
+++ doc-espeak-ng/espeak-ng-src-install.sh
@@ -1,7 +1,7 @@
+#上传文件:http://espeak.sourceforge.net/data/zh_listx.zip 到目录dictsource
 apt-get install gcc
 apt-get install make autoconf automake libtool pkg-config
 tar -zxvf espeak-ng-1.50.tgz
-上传文件:http://espeak.sourceforge.net/data/zh_listx.zip 到目录dictsource
 ./autogen.sh
 ./configure --prefix=/usr --with-extdict-zh=yes
 make LIBDIR=/usr/lib/x86_64-linux-gnu install
