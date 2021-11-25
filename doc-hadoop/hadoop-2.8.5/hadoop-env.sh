7174dd72cee276c7382f55cd3691f48dc17d5449
lhzd863 <zdwst618@126.com>
2021-11-25 11:26:25 +0800

    Create hadoop-env.sh

---
+++ doc-hadoop/hadoop-2.8.5/hadoop-env.sh
@@ -0,0 +1,4 @@
+export JAVA_HOME=/home/coder/jdk/jdk1.8.0_311
+export LD_LIBRARY_PATH=/usr/local/lib
+export HADOOP_HOME=/home/coder/hadoop/hadoop-2.8.5
+export JAVA_LIBRARY_PATH=${JAVA_LIBRARY_PATH}:${HADOOP_HOME}/lib/native/hadoop-lzo:${HADOOP_HOME}/lib/native
